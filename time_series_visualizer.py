f56060d2f24de0aec246d1a2e3a60e8d7cee946b
awault <91794118+awault@users.noreply.github.com>
2024-04-03 12:13:35 -0500

    Import Clean & Format Data

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -5,10 +5,13 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv')
 
 # Clean data
-df = None
+rem_top_outliers = df['value'] <= df['value'].quantile(0.975)
+rem_bot_outliers = df['value'] >= df['value'].quantile(0.025)
+
+df = df[rem_top_outliers & rem_bot_outliers]
 
 
 def draw_line_plot():
